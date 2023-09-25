636b3c1e837031dc6d8f434bb8cc06dd681c1a92
Abhishek Jadhav <abhishekj8345@gmail.com>
2023-09-25 23:23:15 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {  useState } from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState(''); // State variable to track user input
+  const [responseContent, setResponseContent] = useState(null);
+
+  const apiKey = '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';
+
+  const handleSearchClick = () => {
+    const requestData = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: userInput }, // Add user input to messages
+      ],
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ['\n'],
+    };
+
+    const requestDataJSON = JSON.stringify(requestData);
+
+    fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: requestDataJSON,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setResponseContent(data.choices[0].message.content);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Clear the user input after making the API request
+    setUserInput('');
+  };
+
+  const handleInputKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      // Trigger the search button click event when Enter key is pressed
+      handleSearchClick();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>AI Blog Creator</h1>
+      <input
+        placeholder='Enter'
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+        onKeyPress={handleInputKeyPress}
+      />
+      <button onClick={handleSearchClick}>Search</button>
+      {responseContent !== null && <p>{responseContent}</p>}
     </div>
   );
 }
